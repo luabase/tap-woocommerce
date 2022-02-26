d977c6f50da4d5d953c915278b0bf93bebb3713d
Josué M. Sehnem <josue@sehnem.com>
2022-02-26 10:14:48 -0500

    use rest and backoff

--- tap_woocommerce/client.py
+++ tap_woocommerce/client.py
@@ -1,14 +1,12 @@
 """REST client handling, including WooCommerceStream base class."""
 
 import requests
-import copy
-from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
+import backoff
+import logging
+from typing import Any, Dict, Optional, cast
 
-from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
-
-from woocommerce import API
+from singer_sdk.authenticators import BasicAuthenticator
 
 
 class WooCommerceStream(RESTStream):
@@ -18,9 +16,18 @@ class WooCommerceStream(RESTStream):
     def url_base(self) -> str:
         """Return the API URL root, configurable via tap settings."""
         site_url = self.config["site_url"]
-        return f"{site_url}/wp-json/wc/v3"
+        return f"{site_url}/wp-json/wc/v3/"
 
     records_jsonpath = "$[*]"
+
+    @property
+    def authenticator(self) -> BasicAuthenticator:
+        """Return a new authenticator object."""
+        return BasicAuthenticator.create_for_stream(
+            self,
+            username=self.config.get("consumer_key"),
+            password=self.config.get("consumer_secret"),
+        )
 
     def get_next_page_token(
         self, response: requests.Response, previous_token: Optional[Any]
@@ -31,9 +38,10 @@ class WooCommerceStream(RESTStream):
         if total_pages is None:
             return None
 
-        total_pages = int(total_pages)
-        # Only increment the next token if there is another page
-        if previous_token is not None and total_pages > previous_token:
+        if previous_token is None:
+            return 2
+
+        if int(total_pages) > previous_token:
             return previous_token + 1
 
         return None
@@ -52,41 +60,44 @@ class WooCommerceStream(RESTStream):
             params["modified_after"] = start_date.isoformat()
             params["after"] = start_date.isoformat()
             params["date_query_column"] = "post_modified"
-            
+
         return params
 
-    def request_records(self, context: Optional[dict]) -> Iterable[dict]:
-        """Request records from REST endpoint(s), returning response records.
 
-        If pagination is detected, pages will be recursed automatically.
-        """
-        wcapi = API(
-            url=self.config["site_url"],
-            consumer_key=self.config["consumer_key"],
-            consumer_secret=self.config["consumer_secret"],
-            timeout=900
-        )
-        # Start at page 1
-        next_page_token = 1
-        finished = False
-        while not finished:
-            params = self.get_url_params(context, next_page_token=next_page_token)
-            resp = wcapi.get(self.path, params=params)
-            for row in self.parse_response(resp):
-                # Convert empty string to None
-                for (key,value) in row.items():
-                    if value=="":
-                        row[key] = None
-                yield row
-            previous_token = copy.deepcopy(next_page_token)
-            next_page_token = self.get_next_page_token(
-                response=resp, previous_token=previous_token
+    @backoff.on_exception(
+        backoff.expo,
+        (requests.exceptions.RequestException),
+        max_tries=5,
+        factor=2,
+    )
+    def _request_with_backoff(
+        self, prepared_request, context: Optional[dict]
+    ) -> requests.Response:
+        response = self.requests_session.send(prepared_request)
+        if self._LOG_REQUEST_METRICS:
+            extra_tags = {}
+            if self._LOG_REQUEST_METRIC_URLS:
+                extra_tags["url"] = cast(str, prepared_request.path_url)
+            self._write_request_duration_log(
+                endpoint=self.path,
+                response=response,
+                context=context,
+                extra_tags=extra_tags,
             )
-            if next_page_token and next_page_token == previous_token:
-                raise RuntimeError(
-                    f"Loop detected in pagination. "
-                    f"Pagination token {next_page_token} is identical to prior token."
+        if response.status_code in [401, 403, 404]:
+            self.logger.info("Failed request for {}".format(prepared_request.url))
+            self.logger.info(
+                f"Reason: {response.status_code} - {str(response.content)}"
+            )
+            raise RuntimeError(
+                "Requested resource was unauthorized, forbidden, or not found."
+            )
+        elif response.status_code >= 400:
+            raise requests.exceptions.RequestException(
+                f"Failed making request to API: {prepared_request.url} "
+                f"[{response.status_code} - {str(response.content)}]".replace(
+                    "\\n", "\n"
                 )
-            # Cycle until get_next_page_token() no longer returns a value
-            finished = not next_page_token
-
+            )
+        logging.debug("Response received successfully.")
+        return response
